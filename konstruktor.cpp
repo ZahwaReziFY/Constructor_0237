2a57746791ab74368dc6f6dc9650399a58e4c702
ZahwaReziFY <zahwarezi416@gmail.com>
2025-05-17 14:15:55 +0700

    manage access nd add member public

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -5,5 +5,11 @@ using namespace std;
 class mahasiswa {
 private:
     int nim;
-    string nama;   
+    string nama; 
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak(); 
 };
