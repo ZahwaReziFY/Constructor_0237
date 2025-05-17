574fe4b633b9b4086791ad83638b3723dff88aa9
ZahwaReziFY <zahwarezi416@gmail.com>
2025-05-17 13:51:11 +0700

    add member public

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -5,4 +5,14 @@ class buku
     string judul;
 
 public:
-}
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+
+} bukunya;
